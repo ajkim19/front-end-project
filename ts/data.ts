983bcd9584307283c43f829a8ecb32c7967f886b
Aaron Kim <ajkim19@gmail.com>
2025-03-01 02:44:23 +0000

    Write a function to read from localStorage #1

--- ts/data.ts
+++ ts/data.ts
@@ -1,11 +1,26 @@
-// // Creates the data type that will be used for data
-// interface Data {
-//   breedList?: object[];
-//   favoritesList?: object[];
-// }
+// Creates the data type that will be used for data
+interface Data {
+  breedList?: object[];
+  favoritesList?: object[];
+}
+
+// Initializes the data that will be stored in local storage
+let data: Data = {};
 
 // // Saves data to local storage for persistent data
 // function writeData(data: Data): void {
 //   const dataJSON = JSON.stringify(data);
 //   localStorage.setItem('data-storage', dataJSON);
 // }
+
+// Retrieves the data from local storage for the application
+function readData(): Data {
+  const dataJSON = localStorage.getItem('data-storage');
+  if (!dataJSON) {
+    return data;
+  }
+  const dataParsed = JSON.parse(dataJSON) as Data;
+  return dataParsed;
+}
+
+data = readData();
