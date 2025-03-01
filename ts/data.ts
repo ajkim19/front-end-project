4fc6d10b0eef340626724745d6184d9d9f3857e1
Aaron Kim <ajkim19@gmail.com>
2025-03-01 02:42:05 +0000

    Write a function to write to localStorage #1

--- ts/data.ts
+++ ts/data.ts
@@ -1,1 +1,11 @@
-/* exported data */
+// // Creates the data type that will be used for data
+// interface Data {
+//   breedList?: object[];
+//   favoritesList?: object[];
+// }
+
+// // Saves data to local storage for persistent data
+// function writeData(data: Data): void {
+//   const dataJSON = JSON.stringify(data);
+//   localStorage.setItem('data-storage', dataJSON);
+// }
