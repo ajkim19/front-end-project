3a0e0a9dcb6ba1372c0357e61451e831e845a98e
Aaron Kim <ajkim19@gmail.com>
2025-03-22 23:03:20 +0000

    The new rank will correlate with the order it appears in ppData.favoritesList #3

--- ts/main.ts
+++ ts/main.ts
@@ -559,5 +559,4 @@ $divFavoritesList.addEventListener('change', (event: Event) => {
   const newRank = eventTarget.value;
   const favBreed = ppData.favoritesList.splice(Number(oldRank) - 1, 1)[0];
   ppData.favoritesList.splice(Number(newRank) - 1, 0, favBreed);
-  populateFavorites(ppData.favoritesList);
 });
